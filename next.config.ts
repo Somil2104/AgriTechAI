ff919a9ebb8d6cb97aa13e8502e391664753cd81
TheBug <atharvsathe28704@gmail.com>
2024-10-26 21:53:04 +0530

    Corrected Base Path URL

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/agritechai",
+  basePath: "/AgriTechAI",
   output: "export"
 };
 
