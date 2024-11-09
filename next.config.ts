d17cbb1c4bdd1198742fcc3f8bbab3a6b58c4016
Sujoy De <sujoydeofficial@gmail.com>
2024-11-09 19:47:56 +0530

    ci: vercel

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/AgriTechAI",
-  output: "export"
 };
 
 export default nextConfig;
