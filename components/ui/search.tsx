ee5611160c54cad4205c0c92a08d6178186ce75d
Sujoy De <sujoydeofficial@gmail.com>
2024-12-03 18:05:48 +0530

    chore: setup lint

--- components/ui/search.tsx
+++ components/ui/search.tsx
@@ -1,10 +1,7 @@
-'use client';
- 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { usePathname, useSearchParams } from 'next/navigation';
-import { useRouter } from 'next/navigation';
+"use client";
+
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 export default function Search({ placeholder }: { placeholder: string }) {
- 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -13,8 +10,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full bg-white dark:bg-slate-400 rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => {
-        }}
+        onChange={() => {}}
         defaultValue={"search...."}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
