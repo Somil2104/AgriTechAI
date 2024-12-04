605ac8b99fb60c522d4cc73b99973bd0fb9b6f92
Ayush Pandey <ayush12122003@gmail.com>
2024-12-04 10:48:12 +0530

    feat: enhance navbar with className prop and add product pages

---
+++ app/(products)/product/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <h1>
+        All Products
+      </h1>
+    </div>
+  )
+}
