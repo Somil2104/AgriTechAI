f8810f83bbe315ecaae860b51cdebd81815f5577
Shrey5908 <shreyvarshney1@gmail.com>
2024-12-04 14:36:19 +0530

    feat: add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import SubscribeEmailInput from "@/components/SubscribeEmailInput";
-import SwiperComponent from "../components/SwiperComponent";
+import HeroSection from "@/components/hero";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="pt-[calc(4rem+6rem)] w-full">
-        <SwiperComponent />
-      </div>
-      <div className="h-screen w-full" />
+      <HeroSection />
       <SubscribeEmailInput className="bg-accent" />
     </div>
   );
